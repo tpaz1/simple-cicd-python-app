50c7710f793128b5248b06cf99821a68dfa9bd89
Tom Paz <tompazz16@gmail.com>
2021-05-05 01:18:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside with tom paz Docker!!!!!"
+    return "Flask inside with tom hay paz Docker!!!!!"
 
 
 if __name__ == "__main__":
